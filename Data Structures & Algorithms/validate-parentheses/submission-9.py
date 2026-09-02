0c5f4fd3a3e019a5d4f8d02a58c5fb42be575492
Emily Macway <14142461+Emacway@users.noreply.github.com>
2026-09-02 14:14:05 -0700

    Add: validate-parentheses - submission-9

---
+++ Data Structures & Algorithms/validate-parentheses/submission-9.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        # Recursive O(n^2) solution
+        if s == "":
+            return True
+        else:
+            if "{}" in s:
+                newS = s.replace("{}", "")
+                return self.isValid(newS)
+            elif "[]" in s:
+                newS = s.replace("[]", "")
+                return self.isValid(newS)
+            elif "()" in s:
+                newS = s.replace("()", "")
+                return self.isValid(newS)
+            else:
+                return False
+            
